3bee4f528867dadfa4e4ab44f11750f9dd0bd129
hyperSUDO <mtechmo@outlook.com>
2018-04-29 02:53:40 -0400

    The clean up patrol visited this file

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron') 
+const {app, BrowserWindow} = require('electron') // Module to control application life and create native browser window
 const url = require('url') 
 const path = require('path')  
 
@@ -6,9 +6,15 @@ let win
 
 function createWindow() { 
    win = new BrowserWindow({width: 1024, height: 800})
+
+   // Below will need to be uncommented if you want to not have the Menu shown
    win.setMenu(null); 
+
    win.loadURL(url.format ({ 
-    //  pathname: path.join(__dirname, 'apps/info.html'), 
+
+   // Below will need to be uncommented if you want to not use the local file path and instead use web URL instead  
+   //--> pathname: path.join(__dirname, 'apps/info.html'), 
+   
       protocol: 'web:',
       protocol: 'https://play.google.com/music/listen?u=0#', 
       slashes: true
